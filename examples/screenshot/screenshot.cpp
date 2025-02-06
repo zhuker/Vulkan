b00c59af47faa305fb2ad87b0b1f99da50f6ae9d
Alex Zhukov <azhukov@roblox.com>
2025-02-06 03:09:37 -0800

    use LOGE,LOGI instead of std::cerr,cout

--- examples/screenshot/screenshot.cpp
+++ examples/screenshot/screenshot.cpp
@@ -266,14 +266,14 @@ static constexpr int COLOR_FormatSurface                   = 0x7F000789;
 		// Check if the device supports blitting from optimal images (the swapchain images are in optimal format)
 		vkGetPhysicalDeviceFormatProperties(physicalDevice, swapChain.colorFormat, &formatProps);
 		if (!(formatProps.optimalTilingFeatures & VK_FORMAT_FEATURE_BLIT_SRC_BIT)) {
-			std::cerr << "Device does not support blitting from optimal tiled images, using copy instead of blit!" << std::endl;
+            LOGE("Device does not support blitting from optimal tiled images, using copy instead of blit!");
 			supportsBlit = false;
 		}
 
 		// Check if the device supports blitting to linear images
 		vkGetPhysicalDeviceFormatProperties(physicalDevice, VK_FORMAT_R8G8B8A8_UNORM, &formatProps);
 		if (!(formatProps.linearTilingFeatures & VK_FORMAT_FEATURE_BLIT_DST_BIT)) {
-			std::cerr << "Device does not support blitting to linear tiled images, using copy instead of blit!" << std::endl;
+            LOGE("Device does not support blitting to linear tiled images, using copy instead of blit!");
 			supportsBlit = false;
 		}
 
@@ -454,7 +454,7 @@ static constexpr int COLOR_FormatSurface                   = 0x7F000789;
 		}
 		file.close();
 
-		std::cout << "Screenshot saved to disk" << std::endl;
+        LOGI("Screenshot saved to disk");
 
 		// Clean up resources
 		vkUnmapMemory(device, dstImageMemory);
